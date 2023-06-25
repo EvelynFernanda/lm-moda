659485982674ff8768b7c99c2fb64147e231c5de
Evelyn Fernanda <evelyn-cardoso@outlook.com.br>
2023-06-24 21:26:12 -0300

    lm modav1

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.scss";
+import imgMain from "./assets/main.png";
+import FlowerSvg from "./components/FlowerSvg/index";
+import Footer from "./components/Footer/index";
+
+function App() {
+  return (
+    <div className="insta">
+      <div className="insta-content">
+        <FlowerSvg />
+        <img width={150} height={150} src={imgMain} />
+        <h1>LM Moda Feminina</h1>
+        <p>Trabalhamos com encomendas e a pronta entrega.</p>
+        <nav className="insta-contact">
+          <a target={"_blank"} href="https://wa.me/5516996115807">
+            WhatsApp
+          </a>
+        </nav>
+        <p>"Até aqui nos ajudou o SENHOR". (1 Samuel 7:12)</p>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
